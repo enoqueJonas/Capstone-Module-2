d3d3f25afcae2ff5c3c60a847baebd2e469b8423
enoqueJonas <enoque.macanda@gmail.com>
2022-11-30 17:01:32 +0200

    FIX linters

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import renderHome from './modules/views/home.js';
 import '../assets/css/style.css';
-import renderHeader from './modules/views/header.js'
+import renderHeader from './modules/views/header.js';
 
 renderHeader();
 renderHome();
