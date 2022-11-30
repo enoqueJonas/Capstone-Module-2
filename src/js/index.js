ffd5691350169507a10eed40a4484ebe536e529a
Amen TETTEH <tkamen@outlook.com>
2022-11-30 16:44:43 +0000

    Fix conflicts

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,8 @@
 import renderHome from './modules/views/home.js';
 
-import renderHeader from './modules/views/header.js';
-
 import renderFooter from './modules/views/footer.js';
 
 import '../assets/css/main.css';
 
-renderHeader();
 renderHome();
 renderFooter();
--- webpack.config.js
+++ webpack.config.js
@@ -3,47 +3,26 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/js/index.js',
-  mode: 'production',
+  mode: 'development',
+  devtool: 'inline-source-map',
   devServer: {
-    client: {
-      webSocketURL: 'http://127.0.0.1:8080',
-    },
     static: './dist',
-  },
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
   ],
-  optimization: {
-    runtimeChunk: 'single',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(csv|tsv)$/i,
-        use: ['csv-loader'],
-      },
-      {
-        test: /\.xml$/i,
-        use: ['xml-loader'],
       },
     ],
   },
