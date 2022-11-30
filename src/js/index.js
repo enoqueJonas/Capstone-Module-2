13f573ad95cc61743551d6caefc32c868f751b8b
Amen TETTEH <tkamen@outlook.com>
2022-11-30 19:32:41 +0000

    Fix linters errors

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@ import renderHome from './modules/views/home.js';
 import renderHeader from './modules/views/header.js';
 import renderFooter from './modules/views/footer.js';
 import '../assets/css/main.css';
+
 renderHeader();
 renderHome();
 renderFooter();
