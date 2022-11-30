03342d6c163505af334b0c0db44d836781d7f4c6
Amen TETTEH <tkamen@outlook.com>
2022-11-30 16:10:40 +0000

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,7 @@ import renderHeader from './modules/views/header.js';
 import renderFooter from './modules/views/footer.js';
 
 import '../assets/css/main.css';
+
 renderHeader();
 renderHome();
 renderFooter();
