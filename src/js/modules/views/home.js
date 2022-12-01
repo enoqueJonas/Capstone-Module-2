44538299b764f3518c2e02eec77601d590c44c50
Amen TETTEH <tkamen@outlook.com>
2022-12-01 13:20:56 +0000

    Refactor to fix linters errors

--- src/js/modules/views/home.js
+++ src/js/modules/views/home.js
@@ -2,22 +2,39 @@ import apiCall from './api.js';
 import Pokemon from '../models/pokemon.js';
 
 const content = document.getElementById('content');
-const pokemonCard = (pokemon) => {
-  let res = '';
-  res += `
-  <li class="card">
-      <img class="card-image" src="${pokemon.image}"/>
-      <div>
-        <h2 class="card-title">${pokemon.name}</h2>
-        <span><i class="fa fa-user"></i></span>
-      </div> 
-      <span class="card-subtitle">${pokemon.getLikeSentence()} </span></br>
-      <button class="button">Comments</button>
-  </li>
-  `;
-  return res;
+
+const createTag = (tagName, textContent = null, className = null) => {
+  const tag = document.createElement(tagName);
+  tag.textContent = textContent;
+  tag.className = className;
+  return tag;
 };
+
+const pokemonCard = (pokemon) => {
+  const listItem = createTag('li', null, 'card');
+  const cardImage = createTag('img', null, 'card-image');
+  cardImage.src = `${pokemon.image}`;
+  const titleDiv = createTag('div', null, null);
+  const title = createTag('h2', `${pokemon.name}`, 'card-title');
+  const likeIconContainer = createTag('span', null, 'like-icon');
+  const likeIcon = createTag('i', null, 'fa fa-user');
+  likeIconContainer.appendChild(likeIcon);
+  titleDiv.appendChild(title);
+  titleDiv.appendChild(likeIconContainer);
+  const cardSubtitle = createTag('span', `${pokemon.getLikeSentence()}`, 'card-subtitle');
+  const newLigne = createTag('br', null, null);
+  const commentButton = createTag('button', 'Comments', 'comment-button');
+
+  const rowItems = [cardImage, titleDiv, cardSubtitle, newLigne, commentButton];
+
+  for (let j = 0; j < rowItems.length; j += 1) {
+    listItem.appendChild(rowItems[j]);
+  }
+  return listItem;
+};
+
 const displayPokemon = () => {
+  const home = createTag('ul', null, 'list');
   apiCall().then((res) => {
     const result = res.results;
     result.map((result) => {
@@ -30,14 +47,17 @@ const displayPokemon = () => {
             res.sprites.front_default,
             res.types.map((type) => type.type.name).join(', '),
           );
-          content.insertAdjacentHTML('beforeend', pokemonCard(pok));
+          const card = pokemonCard(pok);
+          home.appendChild(card);
         });
       return '';
     });
   });
+  return home;
 };
+
 const renderHome = () => {
-  content.insertAdjacentHTML('beforeend', displayPokemon());
+  content.appendChild(displayPokemon());
 };
 
 export default renderHome;
